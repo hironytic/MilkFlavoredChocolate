703082681fb06e9a1edeab125770c9c8a461a690
Hironori Ichimiya <hiron@hironytic.com>
2021-12-31 12:44:21 +0900

    Remove unused codes

--- Tests/MilkFlavoredChocolateTests/MilkFlavoredChocolateTests.swift
+++ Tests/MilkFlavoredChocolateTests/MilkFlavoredChocolateTests.swift
@@ -3,9 +3,9 @@ import XCTest
 
 final class MilkFlavoredChocolateTests: XCTestCase {
     func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(MilkFlavoredChocolate().text, "Hello, World!")
+        // // This is an example of a functional test case.
+        // // Use XCTAssert and related functions to verify your tests produce the correct
+        // // results.
+        // XCTAssertEqual(MilkFlavoredChocolate().text, "Hello, World!")
     }
 }
